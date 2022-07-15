463b149576e09d29b3550f4647f81f2f21613ed3
marcocalligaris <marcocally@gmail.com>
2022-07-15 16:16:47 +0200

    Reset Milestone 1

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const app = new Vue ({
+  el: '#root',
+  data: {
+    user: {
+      name: 'Elena',
+      avatar: '_io'
+    },
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+      },
+      {
+        name: 'Luigi',
+        avatar: '_4',
+      },
+      {
+        name: 'Marco',
+        avatar: '_5',
+      },
+      {
+        name: 'Annalisa',
+        avatar: '_6',
+      },
+      {
+        name: 'Stefano',
+        avatar: '_7',
+      },
+      {
+        name: 'Marcello',
+        avatar: '_8',
+      },
+    ]
+  }
+})
